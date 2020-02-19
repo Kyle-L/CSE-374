38b75ba6d83bb1bc0c57d3a2d2f5ba880108c445
Kyle <liererkt@gmail.com>
2020-02-19 17:12:26 -0500

    Added CodingAssignment02.

---
+++ Coding Assignments/CodingAssignment02/CodingAssignment02/src/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution s = new Solution();
+		long t = s.nthUglyNumber(1000000000, 2, 217983653, 336916467);
+		System.out.println(t);
+	}
+
+}
